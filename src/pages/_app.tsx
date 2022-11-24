9c7c381a35c062fb101b6644ee0047fe16819c26
Krishna Kumar <dev@krishnaaa.com>
2022-11-24 16:08:08 +0530

    Progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,24 @@ import { ModalsProvider } from "@mantine/modals";
 import { NotificationsProvider } from "@mantine/notifications";
 import type { AppProps } from "next/app";
 import { CookiesProvider } from "react-cookie";
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from "next/router";
+
+const progress = new ProgressBar({
+  size: 2,
+  color: "#2E1739",
+  className: "progress-bar",
+  delay: 100,
+});
+
+if (typeof window !== "undefined") {
+  progress.start();
+  progress.finish();
+}
+
+Router.events.on("routeChangeStart", () => progress.start());
+Router.events.on("routeChangeComplete", () => progress.finish());
+Router.events.on("routeChangeError", () => progress.finish());
 
 import "../styles/globals.css";
 
